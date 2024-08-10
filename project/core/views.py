8a485077ae96ccdb309eb2dc0c8189d0c9fc3ca3
Palun Martinez <palunmartinez@gmail.com>
2024-08-10 17:58:58 -0300

    Feat: Contacto.html. Update: core/

--- project/core/views.py
+++ project/core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.contrib.auth.models import User
 
 def index(request):
-    usuario = User.objects.all()
-    return render(request, 'core/index.html', {'usuario': usuario})
+    return render(request, 'core/index.html')
+
+def contacto(request):
+    return render(request, 'core/contacto.html')
